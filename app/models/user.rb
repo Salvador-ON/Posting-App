f8dff269d6b0b17b54417e2a7b0e6f62a8c3f007
Salvador <salvador.on@outlook.com>
2020-03-27 10:26:26 -0300

    add relationship table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweeets
+
+  has_many :active_relationships, class_name:  "Relationship",
+  foreign_key: "follower_id",
+  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :tweeets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweeets#index"
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
 end
